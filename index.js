326009845da56d9df16c929f9e927ffc25b99682
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-12-08 20:36:23 +0000

    Update

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const appKey = "f817cddb3ec2d6eaae7099c1bbdf4596";
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-let searchTxt = document.getElementById("serch-txt");
-let searchBtn = document.getElementById("serch-btn");
+let searchTxt = document.getElementById("search-txt");
+let searchBtn = document.getElementById("search-btn");
 let city = document.getElementById("city");
 let desc = document.getElementById("desc");
 let temp = document.getElementById("temp");
 let wind = document.getElementById("wind");
 
 searchBtn.addEventListener("click", findWeatherDetails);
-searchTxt.addEventListener("keyup", enterPressed);
+searchTxt.addEventListener("keypress", enterPressed);
 
 function enterPressed(event) {
-  if (event.key === "Enter") {
+  if (event.keyCode == 13) {
+    event.preventDefault();
     findWeatherDetails();
   }
 }
 
 function findWeatherDetails() {
-  if (searchInput.value === "") {
+  if (searchTxt.value === "") {
   
-  }else {
-   getWeather();
+  } else {
+    getWeather();
   }
 }
 
 function getWeather() {
-  let quoteUrl = url + searchTxt.value + "&appid=" + appKey;
+  let quoteUrl = url + searchTxt.value + "&units=metric&appid=" + appKey;
   fetch(quoteUrl, { cache: "no-store" })
-    .then(function(resp) {
-      return resp.json();
+    .then(response => { 
+      return response.json();
     })
-    .then(showWeather);
+    .then((response) => {
+      showWeather(response)
+      console.log(response)
+    })
 };
 
-function showWeather() {
-  temp.innerHTML = resp.main.temp - 273 + "°";
+function showWeather(response) {
+  temp.innerHTML = response.main.temp + '°';
 };
